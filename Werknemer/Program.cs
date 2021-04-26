b578c1d46d04d367a1c9ed79d57649bb75e8ad9c
Jesse <jesvis@bitconsult.be>
2021-04-26 15:40:06 +0200

    Initial_phase

---
+++ Werknemer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Werknemer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Werknemer wn = new Werknemer();
+        }
+    }
+}
---
+++ Werknemer/Werknemer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Werknemer
+{
+    class Werknemer
+    {
+        //Database db;
+        public Werknemer()
+        {
+            //db = new Database();
+        }
+        void Insert()
+        {
+            try
+            {
+                string sql = "insert into werkemers(voornaam,achternaam,stad) values('Jesse','Viskens','Antwerpen')";
+                //db.Query(sql);
+            }
+            catch (Exception e)
+            {
+                //Log error
+                System.IO.File.WriteAllText(@"C:\Error.txt", e.ToString());
+            }
+        }
+        void Delete() { }
+        void Update() { }
+    }
+}
